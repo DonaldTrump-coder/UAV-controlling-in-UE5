5e2168a525c139735bb4603322df109502a9cea4
DonaldTrump-coder <3279179450@qq.com>
2025-09-09 15:25:06 +0800

    add image

--- controller.py
+++ controller.py
@@ -1,15 +1,92 @@
 import airsim
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtGui import QImage
+import numpy as np
+import cv2
 
 class Controller(QThread):
 
     image_signal=pyqtSignal(QImage) #signal to send image
+    status="None"
 
     def __init__(self):
         super().__init__()
-        client = airsim.MultirotorClient()
-        client.confirmConnection()
-        client.enableApiControl(True)
-        client.armDisarm(True)
-        client.takeoffAsync().join() #taking off before controlling
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
+        self.client.armDisarm(True)
+        self.client.takeoffAsync().join() #taking off before controlling
+        self.running=True
+        self.landed=False
+
+    def takeoff(self):
+        self.client.takeoffAsync().join()
+        self.landed=False
+
+    def landing(self):
+        self.client.landAsync().join()
+        self.landed=True
+
+    def set_status(self,status:str):
+        self.status=status
+
+    def forward(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=2, vy=0, vz=0, duration=0.1).join()
+    
+    def backward(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=-2, vy=0, vz=0, duration=0.1).join()
+
+    def move_left(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=0, vy=-2, vz=0, duration=0.1).join()
+
+    def move_right(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=0, vy=2, vz=0, duration=0.1).join()
+
+    def hover(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=0, vy=0, vz=-2, duration=0.1).join()
+
+    def drop(self):
+        self.client.moveByVelocityBodyFrameAsync(vx=0, vy=0, vz=2, duration=0.1).join()
+
+    def turn_left(self):
+        self.client.rotateByYawRateAsync(yaw_rate=-5, duration=0.1).join()
+
+    def turn_right(self):
+        self.client.rotateByYawRateAsync(yaw_rate=5, duration=0.1).join()
+
+    def take_image(self):
+        png_image = self.client.simGetImage("0", airsim.ImageType.Scene)
+        np_img = np.frombuffer(png_image, dtype=np.uint8)
+        frame = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+        # BGR -> RGB
+        rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+
+        # OpenCV → QImage
+        qimg = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        self.image_signal.emit(qimg)
+
+    def run(self):
+        while self.running is True:
+            if self.landed is True:
+                continue
+            if self.status == "None":
+                continue
+            elif self.status == "Forward":
+                self.forward()
+            elif self.status == "Backward":
+                self.backward()
+            elif self.status == "Left":
+                self.move_left()
+            elif self.status == "Right":
+                self.move_right()
+            elif self.status == "Hover":
+                self.hover()
+            elif self.status == "Dropping":
+                self.drop()
+            elif self.status == "Turn Left":
+                self.turn_left()
+            elif self.status == "Turn Right":
+                self.turn_right()
--- ui/GUI.py
+++ ui/GUI.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget,QVBoxLayout,QLabel
 from controller import Controller
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage,QPixmap
 
 class AirSimGUI(QWidget):
     def __init__(self):
@@ -8,30 +9,62 @@ class AirSimGUI(QWidget):
         self.setWindowTitle("Drone Status")
 
         layout=QVBoxLayout(self)
-        self.status=QLabel("Present Button:",self)
+        self.status=QLabel("Present Button: None",self)
         self.image=QLabel(self)
         self.image.setStyleSheet("background-color: lightgray;")
         layout.addWidget(self.status)
         layout.addWidget(self.image)
 
         self.drone=Controller()
+        self.drone.image_signal.connect(self.update_image)
         self.drone.start()
     
     def update_status(self,mode):
         self.status.setText(f"Present Button: {mode}")
 
+    def update_image(self,qimg:QImage):
+        pixmap = QPixmap.fromImage(qimg)
+        self.image.setPixmap(pixmap)
+
     def keyPressEvent(self,event):
         if event.key() == Qt.Key_W:
-            self.update_status("forward")
+            self.update_status("W")
+            self.drone.set_status("Forward")
         elif event.key() == Qt.Key_S:
-            self.update_status("backward")
+            self.update_status("S")
+            self.drone.set_status("Backward")
         elif event.key() == Qt.Key_A:
-            self.update_status("left")
+            self.update_status("A")
+            self.drone.set_status("Left")
         elif event.key() == Qt.Key_D:
-            self.update_status("right")
+            self.update_status("D")
+            self.drone.set_status("Right")
         elif event.key() == Qt.Key_Space:
-            self.update_status("hover")
+            self.update_status("Space")
+            self.drone.set_status("Hover")
         elif event.key() == Qt.Key_Shift:
-            self.update_status("dropping")
+            self.update_status("Shift")
+            self.drone.set_status("Dropping")
         elif event.key() == Qt.Key_L:
-            self.update_status("land")
+            self.update_status("L")
+            self.drone.landing()
+        elif event.key() == Qt.Key_H:
+            self.update_status("H")
+            self.drone.takeoff()
+        elif event.key() == Qt.Q:
+            self.update_status("Q")
+            self.drone.set_status("Turn Left")
+        elif event.key() == Qt.E:
+            self.update_status("E")
+            self.drone.set_status("Turn Right")
+        elif event.key() == Qt.P:
+            self.update_status("P")
+
+    def keyReleaseEvent(self, event):
+        self.update_status("None")
+
+    def closeEvent(self, event):
+        self.drone.landing()
+        self.drone.running=False
+        self.drone.wait()
+        event.accept()
